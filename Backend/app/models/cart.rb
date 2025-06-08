4bfe64bce592351b572a2371d82c5bb2d0731747
Criz <crizvc3@gmail.com>
2025-06-08 17:29:33 -0500

    incrementar y decrementar

--- Backend/app/models/cart.rb
+++ Backend/app/models/cart.rb
@@ -3,11 +3,27 @@ class Cart < ApplicationRecord
   has_many :cart_products, dependent: :destroy
   has_many :products, through: :cart_products
 
-  def agregar_producto(producto_id, cantidad = 1)
-    cart_product = cart_products.find_or_initialize_by(product_id: producto_id)
-    cart_product.cantidad ||= 0
-    cart_product.cantidad += cantidad
-    cart_product.save!
-    cart_product
+  def agregar_producto(product_id, cantidad)
+    item = cart_products.find_by(product_id: product_id)
+  
+    if item
+      nueva_cantidad = item.cantidad + cantidad
+  
+      if nueva_cantidad <= 0
+        item.destroy
+      else
+        # Limitar al stock del producto
+        stock_maximo = item.product.stock
+        item.update(cantidad: [nueva_cantidad, stock_maximo].min)
+      end
+    else
+      producto = Product.find(product_id)
+      cantidad_inicial = [cantidad, producto.stock].min
+  
+      # Solo agrega si hay stock y cantidad positiva
+      if cantidad_inicial > 0
+        cart_products.create(product: producto, cantidad: cantidad_inicial)
+      end
+    end
   end
 end
