11835bca859f19c6e649ce9708ef5ce65ce7a816
nicolledeavila <nicolledeavila3@gmail.com>
2025-06-23 18:06:57 -0500

    vista de categoría implementada y funcionando

---
+++ Backend/app/controllers/public_categories_controller.rb
@@ -0,0 +1,12 @@
+class PublicCategoriesController < ApplicationController
+  skip_before_action :authenticate_user!
+  def index
+    @categories = Category.all
+     render layout: "inicio"
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @products = @category.products
+  end
+end
