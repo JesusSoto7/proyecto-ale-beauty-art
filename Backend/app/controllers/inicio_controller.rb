c8f07d4f7ed83c21eb0a37a00e8a2441a9a92d5c
Criz <crizvc3@gmail.com>
2025-06-27 00:47:29 -0500

    se agregó a vista cliente

--- Backend/app/controllers/inicio_controller.rb
+++ Backend/app/controllers/inicio_controller.rb
@@ -5,10 +5,22 @@ class InicioController < ApplicationController
     @product = Product.order("RAND()").limit(9)
     @admin = User.with_role(:admin).first
     @categories = Category.all
+    @products = @products.where("nombre_producto LIKE ?", "%#{params[:name]}%") if params[:name].present?
+
   end
 
   def all_products
     @products = Product.all
+
+    # Filtros por nombre
+    @products = @products.where("nombre_producto LIKE ?", "%#{params[:name]}%") if params[:name].present?
+
+    # Filtros por precio (mínimo y máximo)
+    @products = @products.where("precio_producto >= ?", params[:min_price]) if params[:min_price].present?
+    @products = @products.where("precio_producto <= ?", params[:max_price]) if params[:max_price].present?
+
+    # Filtros por categoría
+    @products = @products.where(category: params[:category]) if params[:category].present?
   end
 
   def showClient
@@ -17,6 +29,7 @@ class InicioController < ApplicationController
     unless @product
       redirect_to root_path, alert: "Producto no encontrado"
     end
+    @products = @products.where("nombre_producto LIKE ?", "%#{params[:name]}%") if params[:name].present?
   end
   
 end
