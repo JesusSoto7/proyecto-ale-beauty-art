e9c21991a4dc0c42564fe17c6409150de066ce5d
AijaMartinez <168228355+AijaMartinez@users.noreply.github.com>
2025-06-27 23:13:39 -0500

    actualizar status de orden

--- Backend/app/controllers/payments_controller.rb
+++ Backend/app/controllers/payments_controller.rb
@@ -1,6 +1,7 @@
 class PaymentsController < ApplicationController
   layout 'checkout'
   skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :authenticate_user!, only: [:create]
 
   def new
   end
@@ -9,28 +10,51 @@ class PaymentsController < ApplicationController
     require 'mercadopago'
     sdk = Mercadopago::SDK.new(ENV['MERCADOPAGO_ACCESS_TOKEN'])
 
+    puts "======= PARÁMETROS RECIBIDOS ======="
+    pp params.to_unsafe_h
+    puts "Monto recibido: #{params[:transaction_amount]}"
+    puts "===================================="
+
     payment_data = {
-      transaction_amount: params[:transactionAmount].to_f,
+      transaction_amount: params[:transaction_amount].to_f,
       token: params[:token],
-      description: params[:description],
+      description: params[:description] || "Pago",
       installments: params[:installments].to_i,
-      payment_method_id: params[:paymentMethodId],
+      payment_method_id: params[:payment_method_id],
       payer: {
-        email: params[:cardholderEmail],
+        email: params.dig(:payer, :email),
         identification: {
-          type: params[:identificationType],
-          number: params[:identificationNumber]
-        },
-        first_name: params[:cardholderName]
+          type: params.dig(:payer, :identification, :type),
+          number: params.dig(:payer, :identification, :number)
+        }
       }
     }
+
+    puts "======= PAYMENT DATA ENVIADO A MERCADO PAGO ======="
+    pp payment_data
+    puts "==================================================="
 
     payment_response = sdk.payment.create(payment_data)
     payment = payment_response[:response]
 
+    puts "======= RESPUESTA DE PAGO ======="
+    pp payment_response
+    puts "================================="
 
-
+    if payment["status"] == "approved"
+      order = Order.find(params[:order_id])
+        puts "Orden actualizada exitosamente"
+        render json: { redirect_url: pago_realizado_path(order.id) }
+      else
+        puts "Error al actualizar la orden:"
+        pp order.errors.full_messages
+        render json: { error: "No se pudo actualizar la orden", errores: order.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: "El pago fue rechazado", detalles: payment_response[:response] }, status: :unprocessable_entity
+    end
   end
+
 
 
 end
