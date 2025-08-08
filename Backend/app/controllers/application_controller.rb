31684492a5cca3e68651ae9a33a237bbab661205
nicolledeavila <168228839+nicolledeavila@users.noreply.github.com>
2025-08-07 19:45:59 -0500

    integración de internacionalización funcionando

--- Backend/app/controllers/application_controller.rb
+++ Backend/app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :current_cart
   helper_method :current_order
-
+  before_action :set_locale
+    protect_from_forgery with: :exception
   protected
 
   def configure_permitted_parameters
@@ -59,4 +60,12 @@ class ApplicationController < ActionController::Base
     session[:order_id] = order.id
     order
   end
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
+
+    def default_url_options
+      { locale: I18n.locale }
+    end
+    
 end
