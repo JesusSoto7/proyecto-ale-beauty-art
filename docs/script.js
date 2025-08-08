273d46d6de1d2b52cd926c8ed52187ca94fc1edb
Criz <crizvc3@gmail.com>
2025-08-08 14:50:35 -0500

    black mode

--- docs/script.js
+++ docs/script.js
@@ -1,32 +1,103 @@
-// let temaOscuro = false; // Variable global para guardar el estado
+let temaOscuro = false; // Variable global para guardar el estado
 
-// function cambiarTema() {
-//     const btnRecargar = document.getElementById('tema');
-//     const nav = document.querySelector('nav');
-//     const pres = document.querySelectorAll('pre');
-//     const aside = document.querySelector('aside');
+function cambiarTema() {
+    const btnRecargar = document.getElementById('tema');
+    const nav = document.querySelector('nav');
+    const pres = document.querySelectorAll('pre');
+    const aside = document.querySelector('aside');
+    const cards = document.querySelectorAll('.card');
+    const title = document.querySelectorAll('.title');
+    const btnA = document.querySelectorAll('.btn-recargar');
+    const contO = document.querySelectorAll('.content');
+    const btnsC = document.querySelectorAll('.menu-btn');
+    const logo = document.getElementById('logo');
+    const active = document.querySelectorAll('.active');
 
-//     if (!temaOscuro) {
-//         document.body.style.backgroundColor = '#202020';
-//         nav.style.backgroundColor = '#595959';
-//         aside.style.backgroundColor = '#3d3c3c';
-//         aside.style.color = '#ffffff'; // Cambia el color de fondo del aside
-//         pres.forEach(pre => {
-//             pre.style.backgroundColor = '#3d3c3c'; // ejemplo de cambio
-//         });
-//         document.body.style.color = '#ffffff';
+    if (!temaOscuro) {
+        document.body.style.backgroundColor = '#1a1a1a';
+        nav.style.backgroundColor = '#333';
+        nav.style.border = 'none';
+        
+        aside.style.backgroundColor = '#302d2cff';
+        aside.style.border = 'none';
+        aside.style.color = '#ffffff'; // Cambia el color de fondo del aside
+        pres.forEach(pre => {
+            pre.style.backgroundColor = '#3d3c3c'; // ejemplo de cambio
+        });
+        cards.forEach(card => {
+            card.style.backgroundColor = '#3f3b3a';
+            card.style.border = 'none';
+        });
 
-//         btnRecargar.innerHTML = '<i class="fa-solid fa-moon fa-lg"></i>';
-//         temaOscuro = true;
-//     } else {
-//         document.body.style.backgroundColor = '#ffffff';
-//         nav.style.backgroundColor = '';
+        title.forEach(card => {
+            card.style.color = '#ffffff';
+        });
 
-//         document.body.style.color = '#000000';
-//         btnRecargar.innerHTML = '<i class="fa-solid fa-sun fa-lg"></i>';
-//         temaOscuro = false;
-//     }
-// }
+        btnA.forEach(btn => {
+            btn.style.backgroundColor = ''; // Cambia el color de fondo de los botones
+            btn.style.color = '#ffffff';
+        });
+        contO.forEach(cont => {
+            cont.style.backgroundColor = '#302d2cff'; // Cambia el color de fondo de los contenedores
+            cont.style.color = '#ffffff'; // Cambia el color del texto de los contenedores
+        });
+
+        btnsC.forEach(btn => {
+          btn.style.color = '#ffffff'; // Cambia el color de fondo de los botones
+        });
+
+        active.forEach(btn => {
+          btn.style.color = '#333'; // Cambia el color de fondo de los botones activos
+        });
+        document.body.style.color = '#ffffff';
+
+        logo.style.backgroundColor = '#333';
+        logo.style.border = 'none';
+
+        btnRecargar.innerHTML = '<i class="fa-solid fa-moon fa-lg"></i>';
+        temaOscuro = true;
+    } else {
+        document.body.style.backgroundColor = '#ffffff';
+        document.body.style.color = '#000000';
+        nav.style.backgroundColor = '';
+        nav.style.border = '';
+        aside.style.backgroundColor = '';
+        aside.style.color = '';
+        aside.style.border = '';
+
+        contO.forEach(cont => {
+            cont.style.backgroundColor = ''; // Cambia el color de fondo de los contenedores
+            cont.style.color = ''; // Cambia el color del texto de los contenedores
+        });
+
+        pres.forEach(pre => {
+            pre.style.backgroundColor = ''; // ejemplo de cambio
+        });
+        cards.forEach(card => {
+            card.style.backgroundColor = '';
+            card.style.border = '';
+        });
+
+        title.forEach(card => {
+            card.style.color = '';
+        });
+
+        btnA.forEach(btn => {
+            btn.style.backgroundColor = ''; // Cambia el color de fondo de los botones
+            btn.style.color = '';
+        });
+        btnsC.forEach(btn => {
+          btn.style.color = ''; // Cambia el color de fondo de los botones  
+        });
+
+        logo.style.backgroundColor = '';
+        logo.style.border = '';
+
+        document.body.style.color = '#000000';
+        btnRecargar.innerHTML = '<i class="fa-solid fa-sun fa-lg"></i>';
+        temaOscuro = false;
+    }
+}
 
 function cargarContenido(num) {
     fetch(`${num}.html`)
